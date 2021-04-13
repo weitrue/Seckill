c0e226e2580f5cdb68a32fe3a481ee9ec903cb58
wangpeng <hrbxywp@163.com>
2021-04-13 22:35:15 +0800

    feat: 增加Mix(fan-in+fan-out)模式限流器

---
+++ infrastructure/services/local/ratelimiter/fan.go
@@ -0,0 +1,141 @@
+/**
+ * Author: Wang P
+ * Version: 1.0.0
+ * Date: 2021/4/13 下午6:09
+ * Description:
+ **/
+
+package ratelimiter
+
+import (
+	"Seckill/infrastructure/pool/coroutine"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	minRate = 1
+	minSize = 10
+	FanIn   = 1 << 0
+	FanOut  = 1 << 1
+)
+
+type fanInOut struct {
+	sync.RWMutex
+	queueIn   chan coroutine.Task // 用于 Fan-In 模式的队列 queueIn
+	queueOut  chan coroutine.Task // 用于 Fan-Out 模式的队列 queueOut
+	startTime int64               // 开始时间
+	rate      int64               // 流量速度 控制每次操作数据的时间间隔
+	duration  time.Duration
+	closed    int64 // 状态：0表示开启，1表示关闭
+	mode      int
+}
+
+func NewRateLimiter(size, rate int64, mode int) (RateLimiter, error) {
+	// 接收 size、rate、mode 这三个参数，分别用于设置队列缓冲区、流量速度、模式 mode 用来控制三种模式的主要流程
+	modeMask := FanIn | FanOut
+	if mode > modeMask || modeMask&mode == 0 {
+		return nil, errors.New("wrong flag")
+	}
+	if rate < minRate {
+		rate = minRate
+	}
+	if size < minSize {
+		size = minSize
+	}
+	f := &fanInOut{
+		startTime: 0,
+		rate:      rate,
+		duration:  time.Second / time.Duration(rate),
+		closed:    0,
+		mode:      mode,
+	}
+
+	if FanIn&mode != 0 {
+		// Fan-In队列
+		f.queueIn = make(chan coroutine.Task, size)
+	}
+	if FanOut&mode != 0 {
+		// Fan-Out队列
+		f.queueOut = make(chan coroutine.Task, size)
+	}
+	if mode == modeMask {
+		go f.exchange()
+	}
+	return f, nil
+}
+
+func (f *fanInOut) Push(t coroutine.Task) bool {
+	// 给生产者
+	if atomic.LoadInt64(&f.closed) == 1 {
+		// 已关闭 不能推给生产者
+		return false
+	}
+	f.RLock()
+	defer f.RUnlock()
+
+	if atomic.LoadInt64(&f.closed) == 1 {
+		return false
+	}
+	if FanIn&f.mode != 0 {
+		// FanIn模式 非阻塞推送任务到队列
+		select {
+		case f.queueIn <- t:
+			return true
+		default:
+			return false
+		}
+	} else {
+		// FanOut模式 需要一段时间，然后阻塞式推送到队列
+		f.sleep()
+		f.queueOut <- t
+		return true
+	}
+}
+
+func (f *fanInOut) Pop() (coroutine.Task, bool) {
+	// 出队给消费
+	if FanOut&f.mode != 0 {
+		// FanOut模式 直接消费队列
+		task, ok := <-f.queueOut
+		return task, ok
+	} else {
+		// FanIn模式 需要等待一段时间 再消费队列
+		f.sleep()
+		task, ok := <-f.queueIn
+		return task, ok
+	}
+}
+
+func (f *fanInOut) Close() error {
+	// 关闭限流器
+	f.Lock()
+	defer f.Unlock()
+	if atomic.CompareAndSwapInt64(&f.closed, 0, 1) {
+		if f.mode&FanIn != 0 {
+			close(f.queueIn)
+		} else if f.mode == FanOut {
+			close(f.queueOut)
+		}
+	}
+}
+
+func (f *fanInOut) exchange() {
+	// Goroutine以固定速度消费Fan-In队列，将任务放到Fan-Out队列中
+	for t := range f.queueIn {
+		f.sleep()
+		f.queueOut <- t
+	}
+	close(f.queueOut)
+}
+
+func (f *fanInOut) sleep() {
+	now := time.Now().UnixNano()
+	delta := f.duration - time.Duration(now-atomic.LoadInt64(&f.startTime))
+	if delta > time.Millisecond {
+		time.Sleep(delta)
+	}
+	atomic.StoreInt64(&f.startTime, now)
+}
---
+++ infrastructure/services/local/ratelimiter/rate_limiter.go
@@ -0,0 +1,36 @@
+/**
+ * Author: Wang P
+ * Version: 1.0.0
+ * Date: 2021/4/13 下午5:57
+ * Description: 有两种经典模式实现限流器：Fan-In 和 Fan-Out，也叫扇入和扇出。
+				Fan-In 模式
+				Fan-In 模式是一种多对一的模式，即有多个生产者，只有一个消费者。相当于限流算法中的漏桶算法，处理速度取决于消费者的速度。
+
+				这种模式最大的作用是将生产者的数据排队，在消费端以固定速度消费并处理，它也比较适合需要在消费端操作分布式锁的场景。
+				在秒杀系统中控制队列的消费速度，并通过分布式锁和原子操作扣减库存。
+
+				在Go语言中，Channel可以当队列来用，而生产者和消费者就是Goroutine。
+				对于Fan-In模式来说，它可以看作是多个Goroutine生产数据到一个Channel，由一个Goroutine来消费Channel。
+
+
+				Fan-Out 模式
+				Fan-Out是一种一对多的模式，即一个生产者，有多个消费者。它类似于限流算法中的令牌桶算法，处理速度取决于生产者的速度。
+
+				这种模式最大的作用是在源头通过限制生产者的速度，来控制下游系统的压力。
+				由于下游系统偏重业务，涉及 Redis、mysql 等操作，通常每次请求会有一定的延迟。
+				为了提升吞吐量，通常是采用多个Goroutine的来消费队列中的数据，配合连接池并发请求下游系统。
+
+				在高并发系统中，通常会将两者搭配使用，这种方式也叫组合模式。
+				秒杀系统中，用户的抢购请求在获得初步资格后，先通过Fan-In进入队列，由一个Goroutine消费队列，
+				并通过Fan-Out将请求转发给多个Goroutine来并发操作多个商品的库存。
+ **/
+
+package ratelimiter
+
+import "Seckill/infrastructure/pool/coroutine"
+
+type RateLimiter interface {
+	Push(t coroutine.Task) bool  // 提供给生产者推送数据
+	Pop() (coroutine.Task, bool) // 提供给消费者消费数据
+	Close() error                // 关闭限流器
+}
